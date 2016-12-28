9da58628f1d75f7697d91a669ffd2ebaaffd91ef
gwenf <gwenfaraday@gmail.com>
2016-12-28 17:53:34 -0500

    cleaning up

--- server.js
+++ server.js
@@ -4,24 +4,14 @@ var app = express();
 
 var os = require('os');
 
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.get('/api/whoami', function(req,res){
+app.get('*', function(req,res){
     var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-    // os.hostname()
-    // os.platform()
     var returnObj = {
       "ipaddress": ip,
       "language": req.headers["accept-language"].split(',')[0],
       "Software": os.platform() + '; ' + os.hostname()
     }
     res.send(returnObj);
-});
-
-app.get('*', function(req,res){
-    res.send('Please navigate to: /api/whoami');
 });
 
 var port = process.env.PORT || 8080;
